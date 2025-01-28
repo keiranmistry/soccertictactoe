579efb2461d13d7ae0e71e6ae2893e9d3ca1f166
Keiran Mistry <keiranmistry@gmail.com>
2025-01-28 00:11:56 -0500

    amazing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey This App is AMAZING!!
         </a>
       </header>
     </div>
